0b3dfd0e397f7adb7a09a487f79e1141ff718b85
Sanyam Chaudhary <sanyamc@microsoft.com>
2015-01-06 17:00:45 -0800

    Chapter 1 files

---
+++ C1/parsecsv.py
@@ -0,0 +1,52 @@
+
+"""
+Your task is to process the supplied file and use the csv module to extract data from it.
+The data comes from NREL (National Renewable Energy Laboratory) website. Each file
+contains information from one meteorological station, in particular - about amount of
+solar and wind energy for each hour of day.
+
+Note that the first line of the datafile is neither data entry, nor header. It is a line
+describing the data source. You should extract the name of the station from it.
+
+The data should be returned as a list of lists (not dictionaries).
+You can use the csv modules "reader" method to get data in such format.
+Another useful method is next() - to get the next line from the iterator.
+You should only change the parse_file function.
+"""
+import csv
+import os
+
+DATADIR = "C:\Data-Wrangle"
+DATAFILE ="745090.csv"
+
+
+def parse_file(datafile):
+    name = ""
+    data = []
+    with open(datafile,'rb') as f:
+        lines = csv.reader(f,dialect='excel')
+        first = lines.next()
+        name=first[1]
+        headers = lines.next()
+        for line in lines:
+            if line:
+              #  print line
+                data.append(line)
+        #print data
+    # Do not change the line below
+    print data[0][1]
+    return (name, data)
+
+
+def test():
+    datafile = os.path.join(DATADIR, DATAFILE)
+    name, data = parse_file(datafile)
+
+    assert name == "MOUNTAIN VIEW MOFFETT FLD NAS"
+    assert data[0][1] == "01:00"
+    assert data[2][0] == "01/01/2005"
+    assert data[2][5] == "2"
+
+
+if __name__ == "__main__":
+    test()
